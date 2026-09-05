81344c1d05e6f13ce31ed4a2d2a74a77d2d9ccd0
But0o <agustinleonardola@gmail.com>
2026-09-05 17:35:10 -0300

    feat: calculate network address from IP/CIDR using ipaddress

--- experiments/script_interface.py
+++ experiments/script_interface.py
@@ -1,18 +1,22 @@
 import json
 import subprocess
+import ipaddress
 
 resultado = subprocess.run(["ip" , "-j" , "addr"], capture_output=True, text=True)
 interfaces = json.loads(resultado.stdout)
 
 lista_resultado = []
 
-for interfaz in interfaces:
-    for direccion in interfaz["addr_info"]:
+for interfas in interfaces:
+    for direccion in interfas["addr_info"]:
         if direccion["family"] == "inet":
+            ip_cidr = direccion["local"] + "/" + str(direccion["prefixlen"])
+            interfaz = ipaddress.ip_interface(ip_cidr)
             lista_resultado.append({
-                "interfas": interfaz["ifname"],
+                "interfas": interfas["ifname"],
                 "ip": direccion["local"],
-                "cidr": direccion["prefixlen"]
+                "cidr": direccion["prefixlen"],
+                "red" : str(interfaz.network)
             })
 
 print(lista_resultado)
