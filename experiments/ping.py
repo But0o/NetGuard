b7de0c015a016c7befb7273325a3cc329fdc8176
But0o <agustinleonardola@gmail.com>
2026-09-08 02:11:38 -0300

    feat: add timeout, packet loss and network host discovery to ping function

--- experiments/ping.py
+++ experiments/ping.py
@@ -1,9 +1,10 @@
 import subprocess
 import re
+import ipaddress
 
-def hacer_ping(ip):
+def hacer_ping(ip, timeout=1):
     resultado = subprocess.run(
-        ["ping", "-c", "1", ip],
+        ["ping", "-c", "4", "-W", str(timeout), ip],
         capture_output=True,
         text=True
     )
@@ -13,13 +14,22 @@ def hacer_ping(ip):
 
     texto = resultado.stdout
     ttl = re.search(r"ttl=(\d+)", texto)
-    tiempo = re.search(r"tiempo=(\d+\.\d+)", texto)
+    tiempo = re.search(r"rtt min/avg/max/mdev = (\d+\.\d+)/(\d+\.\d+)/(\d+\.\d+)/(\d+\.\d+)", texto)
+    perdida = re.search(r"(\d+)% packet loss", texto)
 
     return {
         "ip": ip,
         "activo": True,
         "ttl": int(ttl.group(1)),
-        "tiempo_ms": float(tiempo.group(1))
+        "tiempo_ms": float(tiempo.group(2)),
+        "perdida" : float(perdida.group(1))
     }
 
-print(hacer_ping("192.168.1.100"))
+red = ipaddress.ip_network("192.168.1.0/28")
+
+for host in red.hosts():
+    ip_texto = str(host)
+    resultado_ping = hacer_ping(ip_texto)
+    
+    if resultado_ping["activo"] == True:
+        print(resultado_ping)
